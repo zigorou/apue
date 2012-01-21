d5fe10d10c8d61c57e60d137e00ee4a2e4a00358
Toru Yamaguchi <zigorou@cpan.org>
2012-01-21 22:54:07 +0900

    added

---
+++ 1/getcputc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "error.h"
+
+int main(int argc, char **argv) {
+    int c;
+
+    while ( ( c = getc(stdin) ) != EOF ) {
+        if ( putc(c, stdout) == EOF )
+            err_sys("output error");
+    }
+
+    if (ferror(stdin))
+        err_sys("input error");
+
+    exit(0);
+}
